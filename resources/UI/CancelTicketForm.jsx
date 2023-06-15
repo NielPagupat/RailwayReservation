9d2444fa9b893a6b32b5f6c160c7a34eb5d3403d
grixxr <cambarijan.lancestuart>
2023-06-15 18:53:18 +0800

    Train Receipt

--- resources/UI/CancelTicketForm.jsx
+++ resources/UI/CancelTicketForm.jsx
@@ -7,10 +7,10 @@ export default function CancelTicket() {
     <>
       <h1 style={{
       fontFamily: 'Teko',
-      marginLeft: 30
+      marginTop:50
       }}>Cancel Ticket</h1>
-        <form style={{marginLeft:30, marginTop:-10}} action="/cancelTicket" method='Post'>
-        <TextField sx={{marginBottom:1}}name='cancelT' id="outlined-basic" label="Insert Ticket ID" variant="outlined" size="small"/><br/>
+        <form style={{marginTop:-15}} action="/cancelTicket" method='Post'>
+        <TextField sx={{marginBottom:2}}name='cancelT' id="outlined-basic" label="Insert Ticket ID" variant="outlined" size="small"/><br/>
         <Button sx={{backgroundColor: 'red'}} type="submit" variant="contained">Cancel Ticket</Button>
       </form>
     </>
