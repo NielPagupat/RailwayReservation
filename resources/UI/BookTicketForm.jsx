08017003dfd64e1fb1c317381a088bdd6690d493
NielPagupat <niel.pagupat.2016@gmail.com>
2023-06-15 14:27:18 +0800

    useEffect added

--- resources/UI/BookTicketForm.jsx
+++ resources/UI/BookTicketForm.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import cryptoRandomString from 'crypto-random-string'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button';
 import '@fontsource/teko/500.css';
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box/Box';
+import Typography  from '@mui/material/styles/createTypography';
 
 export default function BookTicketForm() {
   let Curdate = new Date().toJSON().slice(0, 10);
@@ -12,32 +14,88 @@ export default function BookTicketForm() {
   const [source, setSource] = useState();
   const [train, setTrain] = useState();
   const [sched, setSched] = useState();
+  const [destination, setDest] = useState();
+  const [uid, setUid] = useState(cryptoRandomString({length:10}));
+  const [cat, setCat] = useState();
+  const [fare, setFare] = useState();
+  const [fname, setName] = useState();
+  const [age, setAge] = useState();
+  const [sex, setSex] = useState();
+  const [address, setAddress] = useState();
   
+  
+  //Modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+  const chkDest = (ev) => {
+    setDest(ev.target.value);
+  }
 
   const chkTrain = (ev) => {
     setTrain(ev.target.value);
   }
+  
+  useEffect(()=>{
+    if (train == 'T01' || train == 'T06') {
+      setSched('AM')
+    }
+    if (train == 'T02' || train == 'T07') {
+      setSched('PM1')
+    }
+    if (train == 'T03' || train == "T08") {
+      setSched('PM2')
+    }
+    if (train == 'T04' || train == "T09") {
+      setSched('EVE1')
+    }
+    if (train == 'T05' || train == "T10") {
+      setSched('EVE2')
+    }
+  }, train);
 
-  if (train == 'T01' || train == 'T06') {
-    document.querySelector('#sched').value = 'AM';
+ useEffect(()=>{
+  if (cat == 1) {
+    setFare('200')
+  } else if (cat == 2) {
+    setFare('150')
   }
-  if (train == 'T02' || train == 'T07') {
-    document.querySelector('#sched').value = 'PM1';
+ }, cat)
+
+  const chkFare = (ev) => {
+    setTime(ev.target.value);
   }
-  if (train == 'T03' || train == "T08") {
-    document.querySelector('#sched').value = 'PM2';
+
+  const chkFname = (ev) => {
+    setName(ev.target.value);
   }
-  if (train == 'T04' || train == "T09") {
-    document.querySelector('#sched').value = 'EVE1';
+
+  const chkAge = (ev) => {
+    setAge(ev.target.value);
   }
-  if (train == 'T05' || train == "T10") {
-    document.querySelector('#sched').value = 'EVE2';
+
+  const chkSex = (ev) => {
+    setSex(ev.target.value);
+  }
+
+  const chkAddress = (ev) => {
+    setAddress(ev.target.value)
+  }
+
+  const chkCat = (ev) => {
+    setCat(ev.target.value);
   }
   
   const chkSched = (ev) => {
-    setSched(ev.target.value)
+  setSched(ev.target.value)
+  console.log('changed');
   }
 
+  const chkUID = (ev) => {
+    setUid(ev.target.value);
+  }
   const chkSource = (ev) => {
     setSource(ev.target.value);
   }
@@ -111,8 +169,8 @@ export default function BookTicketForm() {
       marginLeft: 30,
       marginBottom: 10
       }}>Book Ticket</h1>
-    <form style={{marginLeft: 30}}action='/bookTicket' method='Post'>
-      <input type="text" name='uid' value={cryptoRandomString({length:10})} hidden/>
+    <form style={{marginLeft: 30}}action='/bookTicket' method='Post' id='BookTicket'>
+      <input type="text" name='uid' value={uid} hidden onChange={chkUID}/>
 
       <TextField sx={{marginBottom: 3}} name='bookDt' type='date' size="small" value={date} onChange={chkDate}/><br/>
       
@@ -134,7 +192,7 @@ export default function BookTicketForm() {
         <option value="Davao">Davao</option>
       </select>
       
-      <select style={{padding: 10}} name="destination" id="destination">
+      <select style={{padding: 10}} name="destination" id="destination" value={destination} onChange={chkDest}>
         <option value="dest" disabled selected>Destination</option>
         <option value="CDO">CDO</option>
         <option value="Malaybalay">Malaybalay</option>
@@ -159,28 +217,60 @@ export default function BookTicketForm() {
         <option value="T09" class='routeB'>Hyperion</option>
         <option value="T10" class='routeB'>Katipunan</option>
       </select>
-
-      <select style={{padding: 10, marginRight: 10, marginBottom: 20}} name="sched" id="sched">
+      
+      
+     
+      <select style={{padding: 10, marginRight: 10, marginBottom: 20}} name="sched" id="sched" value={sched} onChange={chkSched}>
         <option value="sched" disabled selected>Schedule</option>
         <option value="AM">AM</option>
         <option value="PM1">PM1</option>
         <option value="PM2">PM2</option>
         <option value="EVE1">EVE1</option>
         <option value="EVE2">EVE2</option>
-      </select><br/>
+    </select>
+    
+    
+    <br/>
       
-      <select style={{padding: 10, marginBottom: 30, marginRight:10, width:'7.5vw'}} name="cat" id="cat">
+      <select style={{padding: 10, marginBottom: 30, marginRight:10, width:'7.5vw'}} name="cat" id="cat" value={cat} onChange={chkCat}>
         <option value="" disabled selected>Seat Category</option>
         <option value="1">AC seat</option>
         <option value="2">Gen seat</option>
       </select>
-      <TextField sx={{width: '5.6vw'}} name='time' type="text" id="outlined-basic" label="Time" variant="outlined" size="small" /><br/>
-      <TextField sx={{marginBottom:1, width:'21.5vw'}} name='name' type="text" id="outlined-basic" label="Name" variant="outlined" size="small" /><br/>
-      <TextField sx={{width:'5vw', marginRight:1, marginBottom:1}} name='age' type="text" id="outlined-basic" label="Age" variant="outlined" size="small" />
-      <TextField sx={{width:'4vw'}} name='sex' type="text" id="outlined-basic" label="Sex" variant="outlined" size="small" /><br/>
-      <TextField sx={{width:'21.5vw', marginBottom:1}} name='address' type="text" id="outlined-basic" label="Address" variant="outlined" size="small" /><br/>
-      <Button sx={{marginBottom:2}} type="submit" variant="contained">Book Ticket</Button>
+
+      <TextField sx={{width: '5.6vw'}} name='Fare' type="text" id="outlined-basic" variant="outlined" size="small" value={fare} onChange={chkFare} /><br/>
+      <TextField sx={{marginBottom:1, width:'21.5vw'}} name='name' type="text" id="outlined-basic" label="Name" variant="outlined" size="small" value={fname} onChange={chkFname}/><br/>
+      <TextField sx={{width:'5vw', marginRight:1, marginBottom:1}} name='age' type="text" id="outlined-basic" label="Age" variant="outlined" size="small" value={age} onChange={chkAge}/>
+      <TextField sx={{width:'4vw'}} name='sex' type="text" id="outlined-basic" label="Sex" variant="outlined" size="small" value={sex} onChange={chkSex}/><br/>
+      <TextField sx={{width:'21.5vw', marginBottom:1}} name='address' type="text" id="outlined-basic" label="Address" variant="outlined" size="small" value={address} onChange={chkAddress}/><br/>
+      {/*<Button sx={{marginBottom:2}} type="submit" variant="contained">Book Ticket</Button>*/}
+      <Button onClick={handleOpen}>BookTicket</Button>
+      <Modal open={open} 
+             onClose={handleClose}
+             sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <Box sx={{width:350, height:550, bgcolor: 'success.main'}}>
+              <p>UID: {uid}</p>
+              <p>Booked Date: {date} </p>
+              <p>Route: {route}</p>
+              <p>Source: {source} </p>
+              <p>Destination: {destination} </p>
+              <p>Train NO.: {train} </p>
+              <p>Schedule: {sched} </p>
+              <p>Fare: {fare}</p>
+              <p>Seating Category: {cat} </p>
+              <p>Name: {fname} </p>
+              <p>Age: {age} </p>
+              <p>Sex: {sex} </p>
+              <p>Address: {address} </p>
+              <Button sx={{marginBottom:2}} type="submit" form='BookTicket' variant="contained">Book Ticket</Button>
+            </Box>
+      </Modal>
+      
     </form>
+
+
+
     </>
   )
 }
+
